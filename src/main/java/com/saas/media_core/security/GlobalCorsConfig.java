e0bfb721612e073492e1c7f899907deee0d76962
Mofeed Tohma <your-email@gmail.com>
2026-08-31 19:43:34 +0300

    Fix ConflictingBeanDefinitionException by renaming to GlobalCorsConfig

--- src/main/java/com/saas/media_core/security/GlobalCorsConfig.java
+++ src/main/java/com/saas/media_core/security/GlobalCorsConfig.java
@@ -1,38 +1,33 @@
 package com.saas.media_core.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Configuration
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsConfig {
+
+@Configuration("securityGlobalCorsConfig") 
+public class GlobalCorsConfig {
 
     @Bean
     @SuppressWarnings("java:S5122")
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean<CorsFilter> customCorsFilterRegistration() {
         CorsConfiguration config = new CorsConfiguration();
         
-        
         config.setAllowCredentials(true);
-        
-        
-        config.addAllowedOriginPattern("*");
-        
-        
+        config.addAllowedOriginPattern("*"); 
         config.addAllowedHeader("*");
-        
-        
-        config.addAllowedMethod("*");
+        config.addAllowedMethod("*"); 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); 
+        return bean;
     }
 }
